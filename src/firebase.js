f6c28e5852e3ef9955aca40f13a9a9d09f506c81
Neil <aneil2642@gmail.com>
2021-03-27 16:09:22 -0400

    added firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDjEEVdxV1JCVoHLtxz43aqccYTZx0Ast0",
+    authDomain: "uva-hackathon.firebaseapp.com",
+    projectId: "uva-hackathon",
+    storageBucket: "uva-hackathon.appspot.com",
+    messagingSenderId: "460907383800",
+    appId: "1:460907383800:web:cc46ff108a44f0b608e40d"
 
 })
 

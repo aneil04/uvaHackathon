f25f52ad1375fb600ef849a1de85bc1775a234e0
nihalbo <nihal.boina@gmail.com>
2021-03-28 02:17:10 -0400

    yourwelcome!

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import "../styles/styles.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
 import Signup from './Signup'
 import { AuthProvider } from '../contexts/AuthContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -9,6 +18,17 @@ import PrivateRoute from './PrivateRoute'
 export default function App() {
   return (
     <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <Container>
+      <a class="navbar-brand" href="/home">Home</a>
+        <a class="navbar-brand" href="#">Give Help</a>
+        <a class="navbar-brand" href="#">Get Help</a>
+        <a class="navbar-brand" href="/login">Login</a>
+        <a class="navbar-brand" href="/logout">Logout</a>
+        <a class="navbar-brand" href="/signup">Sign up</a>
+        <a class="navbar-brand" href="#">Edit Profile</a>
+      </Container>
+    </nav>
       <AuthProvider>
         <Router>
           <Switch>

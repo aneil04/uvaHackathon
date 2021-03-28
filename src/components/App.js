bf348f64c12a3ed70b6bb0d2988b6c26b647ac53
Neil <aneil2642@gmail.com>
2021-03-28 11:54:51 -0400

    stuff

--- src/components/App.js
+++ src/components/App.js
@@ -16,18 +16,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import Login from './Login'
 import PrivateRoute from './PrivateRoute'
-import GiveHelp from './GiveHelp'
-import GetHelp from './GetHelp'
-import homepage from './homepage'
 
 export default function App() {
   return (
     <div>
         <nav class="navbar navbar-dark bg-primary">
           <div class= "container-fluid ">
-                <Button class="navbar-brand" href="/home">Home</Button>
-                <Button class="navbar-brand" href="/givehelp">Give Help</Button>
-                <Button class="navbar-brand" href="/gethelp">Get Help</Button>
                 <Button class="navbar-brand" href="/login">Login</Button>
                 <Button class="navbar-brand" href="/logout">Logout</Button>
                 <Button class="navbar-brand" href="/signup">Sign up</Button>
@@ -39,9 +33,6 @@ export default function App() {
             <PrivateRoute exact path="/" component={Dashboard} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
-            <Route path="/getHelp" component={GetHelp} />
-            <Route path="/giveHelp" component={GiveHelp} />
-            <Route path ="/home" component={homepage} />
           </Switch>
         </Router>
       </AuthProvider>
